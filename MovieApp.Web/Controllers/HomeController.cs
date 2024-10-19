73fbd77a8089ac29256c8e86d6e1c235944f90bd
Ahmet Selim KISA <ahmet---ultraaslan@hotmail.com>
2024-10-19 21:28:57 +0300

    Add project files.

---
+++ MovieApp.Web/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieApp.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieApp.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            string filmBasligi = "film başlığı";
+            string filmAciklama = "filmin açıklaması";
+            string filmYonetmen = "filmin yönetmen adı";
+            string[] oyuncular = { "oyuncu 1", "oyuncu 2", "oyuncu 3", "oyuncu 4" };
+
+            //ViewBag.FilmBasligi = filmBasligi;
+            //ViewBag.FilmAciklamasi = filmAciklama;
+            //ViewBag.FilmYonetmeni = filmYonetmen;
+            //ViewBag.Oyuncular = oyuncular;
+
+            var m = new Movie();
+            m.Tilte = filmBasligi;
+            m.Description = filmAciklama;
+            m.Director = filmYonetmen;
+            m.Cast = oyuncular;
+
+            return View(m);
+        }
+        //public string Index()
+        //{
+        //    return "Anasayfa";
+        //}
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ MovieApp.Web/Controllers/MoviesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieApp.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieApp.Web.Controllers
+{
+    public class MoviesController : Controller
+    {
+        //localhost:5000/movies
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult List()
+        {
+            var filmListesi = new List<Movie>()
+            {
+                new Movie { Tilte = "Film 1", Description = "Açıklama 1", Director = "Yönetmen 1", Cast = new string[] { "oyuncu 1", "oyuncu 2" },ImageUrl = "1.jpg"},
+                new Movie { Tilte = "Film 2", Description = "Açıklama 2", Director = "Yönetmen 2", Cast = new string[] { "oyuncu 1", "oyuncu 2" },ImageUrl = "2.jpg" }, 
+                new Movie { Tilte = "Film 3", Description = "Açıklama 3", Director = "Yönetmen 3", Cast = new string[] { "oyuncu 1", "oyuncu 2" },ImageUrl = "3.jpg" },
+                new Movie { Tilte = "Film 4", Description = "Açıklama 4", Director = "Yönetmen 4", Cast = new string[] { "oyuncu 1", "oyuncu 2" },ImageUrl = "4.jpg" }
+            };
+            return View("Movies", filmListesi);
+        }
+        //localhost:5000/movies/details
+        public string Details()
+        {
+            return "Film Detayı";
+        }
+    }
+}
