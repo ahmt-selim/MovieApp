3aa6c6910ab8a92bf8557985a20e40e03d6e871d
Ahmet Selim KISA <ahmet---ultraaslan@hotmail.com>
2024-11-16 23:12:05 +0300

    Linq sorguları ile ToList(), First(), FirstOrDefault() ile veri getirme yöntemleri uydulandı.

--- LINQSamples/Program.cs
+++ LINQSamples/Program.cs
@@ -4,6 +4,11 @@ using System.Linq;
 
 namespace LINQSamples
 {
+    class ProductModel
+    {
+        public string Name { get; set; }
+        public decimal? Price { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
@@ -11,11 +16,19 @@ namespace LINQSamples
             using (var db = new NorthwindContext())
             {
                 //var products = db.Products.ToList();
-                var products = db.Products.Select(p => p.ProductName).ToList();
-                foreach (var p in products)
-                {
-                    Console.WriteLine(p);
-                }
+                //var products = db.Products.Select(p => p.ProductName).ToList();
+                // birden fazla kolon getirmek istediğimizde bu şekilde kullanılır.
+                //var products = db.Products.Select(p => new { p.ProductName, p.UnitPrice }).ToList();
+
+                //var products = db.Products.Select(p => 
+                //new ProductModel{ Name=p.ProductName, Price=p.UnitPrice }).ToList();
+                //foreach (var p in products)
+                //{
+                //    Console.WriteLine(p.Name + " " + p.Price);
+                //}
+
+                var product = db.Products.First();//İlk satırı döner. Eğer bir satır gelmesse hata vereceği için FirstOrDefault() kullanılır.
+                Console.WriteLine(product.ProductName + " " + product.UnitPrice);
             }
             Console.ReadLine();
         }
