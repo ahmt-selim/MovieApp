4a326b07f1bb30e1946f7f84973fa1bd8a198ab0
Ahmet Selim KISA <ahmet---ultraaslan@hotmail.com>
2024-12-01 22:14:22 +0300

    User ekleme formu farklı Validation Attributes değerleri eklendi.

--- MovieApp.Web/Models/UserModel.cs
+++ MovieApp.Web/Models/UserModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,12 +9,26 @@ namespace MovieApp.Web.Models
     public class UserModel
     {
         public int UserId { get; set; }
+        [Required]
+        [StringLength(10, ErrorMessage = "UserName için 10 karakterden fazla olamaz")]
         public string UserName { get; set; }
+        [Required]
+        //Aşağıdaki gibi kullanımlarda 0:Property ismi, 2:Max değer, 1:Min değer olarak kullanılır.
+        [StringLength(25, ErrorMessage = "{0} karakter uzunluğu {2}-{1} arasında olmalıdır", MinimumLength =3)]
         public string Name { get; set; }
+        [Required]
+        [EmailAddress(ErrorMessage = "Geçerli bir email giriniz")]
         public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))]//Password properttsi ile karşılaştırılır. Aynı değil ise hata verir.
         public string RePassword { get; set; }
+        [Url]
         public string Url { get; set; }
+        [Range(1900,2010)]
         public int BirthYear { get; set; }
     }
 }
